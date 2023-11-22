d07369c7faa28f2448786b8d7ce721850e603f93
tere <125310682+terecast@users.noreply.github.com>
2023-11-21 21:23:31 -0600

    rutas

---
+++ backend/routes/tareasRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const {getTarea, crearTarea, modificarTarea, deleteTarea} = require('../controller/tareasController')
+
+//Opciones mas simple
+//router.route('/').get(getTarea).post(crearTarea)
+//router.route('/').put(modificarTarea).delete(eliminarTarea)
+
+//OBTENER TAREAS
+router.get('/', getTarea)
+    
+//CREAR TAREAS
+router.post('/', crearTarea)
+   
+//MODIFICAR TAREAS
+router.put('/:id', modificarTarea) 
+  
+//ELIMINAR TAREAS
+router.delete('/:id', deleteTarea) 
+
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,7 @@ const express = require("express")
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
+
+app.use('/api/tareas', require('./routes/tareasRoutes'))
+
 app.listen(port, ()=> console.log(`Server iniciado en el puerto ${port}`))
